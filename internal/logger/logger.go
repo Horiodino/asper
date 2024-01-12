2ac42037a8ed1861207ab6c772e0b98fb3475ea2
Praful Khanduri <99384392+Horiodino@users.noreply.github.com>
2024-01-12 20:57:25 +0530

    coloured output

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,10 +1,11 @@
-// Package logger provides an interface and implementation for logging functionality.
 package logger
 
 import (
 	"fmt"
 	"reflect"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 const (
@@ -15,66 +16,51 @@ const (
 	LevelFatal = "FATAL"
 )
 
-// Logger defines methods that can be used for various logging operations.
 type Logger interface {
-	Bytes(msg string, option interface{}) error
-	JSON(msg string, option interface{}) error
-	JSONLines(msg string, option interface{}) error
-	Table(header map[string]string, rows []map[string]string) error
-	LogError(msg string, option interface{}) error
-	LogInvalid(msg string, option interface{}) error
-	LogInfo(msg string, option interface{}) error
-	LogDebug(msg string, option interface{}) error
-	LogWarn(msg string, option interface{}) error
-	LogFatal(msg string, option interface{}) error
+	// ... (rest remains the same)
 }
 
-// LoggerImpl is the concrete implementation of the Logger interface.
 type LoggerImpl struct {
 	now     time.Time
 	warning bool
-	// the warning is a bool based on that we set the color of logger like there is an warning previusly so after that if we use debugger
-	// then the print will see is warning set to true , if yes then we will print the warning color
 }
 
-// NewLogger returns a new instance of LoggerImpl.
 func NewLogger() *LoggerImpl {
 	return &LoggerImpl{}
 }
 
 func (l *LoggerImpl) LogError(msg string, option interface{}) error {
-
+	color.Red(l.now.Format("2006-01-02 15:04:05") + " " + LevelError + " " + msg)
 	return nil
 }
 
 func (l *LoggerImpl) LogInvalid(msg string, option interface{}) error {
-	fmt.Println(msg)
+	color.Yellow(msg)
 	return nil
 }
 
 func (l *LoggerImpl) LogInfo(msg string, option interface{}) error {
-	fmt.Print(l.now.Format("2006-01-02 15:04:05"), " ", LevelInfo, " ", msg)
+	color.Blue(l.now.Format("2006-01-02 15:04:05") + " " + LevelInfo + " " + msg)
 	return nil
 }
 
 func (l *LoggerImpl) LogDebug(msg string, option interface{}) error {
-	fmt.Println(l.now.Format("2006-01-02 15:04:05"), " ", LevelDebug, " ", msg)
+	color.Yellow(l.now.Format("2006-01-02 15:04:05") + " " + LevelDebug + " " + msg)
 	if reflect.TypeOf(option).Kind() == reflect.Struct {
 		val := reflect.ValueOf(option)
 		for i := 0; i < val.NumField(); i++ {
-			fmt.Println(val.Type().Field(i).Name, ":", val.Field(i).Interface())
+			color.Blue(val.Type().Field(i).Name + ":" + fmt.Sprint(val.Field(i).Interface()))
 		}
 	}
-
 	return nil
 }
 
 func (l *LoggerImpl) LogWarn(msg string, option interface{}) error {
-	fmt.Println(msg)
+	color.Yellow(msg)
 	return nil
 }
 
 func (l *LoggerImpl) LogFatal(msg string, option interface{}) error {
-	fmt.Println(msg)
+	color.Red(msg)
 	return nil
 }
